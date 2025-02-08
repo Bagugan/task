90fa7bfee18b2e36f81c2426130cb11988041cdf
p-23 <testp23@mail.ru>
2025-02-08 12:17:42 +0300

    first commit

--- П23.php
+++ П23.php
@@ -10,13 +10,3 @@ if ($password === $enteredpassword) {
 } else { 
    echo 'пароль не принят';
 }
-
-
-
-echo "# task" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Bagugan/task.git
-git push -u origin main
